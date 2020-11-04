ea3da93bd3b58a540b93891b707b48d9d6c339ea
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-11-04 17:50:22 -0600

    added firs ttest and note how to stop jest so i can do next coand

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // npm init to create package.json
 //to add .gitignore - conand line: touch .gitignore
 // npm install inquirer to get the node_modules and package-lock.son (see 9.3.4 for details)
+//npm run test to run test - note make have to reinstal jest while in this file. 
+//must controle C everytime to stop the jest command (or most other comands that wont stop)
