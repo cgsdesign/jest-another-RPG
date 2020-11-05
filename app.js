350ee08ba06d2973a9afce6963da376e9736ea52
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-11-04 18:45:15 -0600

    commiting hidden files maybe

--- app.js
+++ app.js
@@ -2,4 +2,4 @@
 //to add .gitignore - conand line: touch .gitignore
 // npm install inquirer to get the node_modules and package-lock.son (see 9.3.4 for details)
 //npm run test to run test - note make have to reinstal jest while in this file. 
-//must controle C everytime to stop the jest command (or most other comands that wont stop)
+//must control C everytime to stop the jest command (or most other comands that wont stop)
